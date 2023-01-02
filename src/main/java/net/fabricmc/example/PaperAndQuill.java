867a6861b51137498d8482a101151056b803092f
parsa shahmaleki <parsampsh@gmail.com>
2023-01-02 16:55:16 +0330

    added `paper and quill` item to the game

--- src/main/java/net/fabricmc/example/PaperAndQuill.java
+++ src/main/java/net/fabricmc/example/PaperAndQuill.java
@@ -1,21 +1,24 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PaperAndQuill implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("paper-and-quill");
+
+	public static final PaperAndQuillItem PAPER_AND_QUILL_ITEM = Registry.register(
+			Registry.ITEM,
+			new Identifier("paper_and_quill", "paper_and_quill"),
+			new PaperAndQuillItem(new FabricItemSettings().group(ItemGroup.MISC))
+	);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		//
 	}
 }
---
+++ src/main/java/net/fabricmc/example/PaperAndQuillItem.java
@@ -0,0 +1,21 @@
+package net.fabricmc.example;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class PaperAndQuillItem extends Item {
+    public PaperAndQuillItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        playerEntity.playSound(SoundEvents.ITEM_BOOK_PAGE_TURN, 1.0F, 1.0F);
+        return TypedActionResult.success(playerEntity.getStackInHand(hand));
+    }
+}
